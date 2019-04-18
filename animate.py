c9ce363fa984310392d38c1bfc7186bd00a48372
MatthewGoree <matthewgoree@utexas.edu>
2019-04-18 02:25:37 -0500

    updated animate.py

--- animate.py
+++ animate.py
@@ -4,10 +4,18 @@ from mathutils import Vector
 import numpy as np
 import os
 
+DATA_PATH = r"C:\Users\mattg\OneDrive\Documents\Design\DesignRepo\2mag8.csv"
+RENDER_PATH = r"C:\Users\mattg\OneDrive\Documents\Design\DesignRepo\output.avi"
+AUTO_RENDER = True
+
 # useful shortcut
 scene = bpy.context.scene
 
-# this shows you all objects in scene
+#render settings
+scene.render.image_settings.file_format = 'AVI_RAW'
+scene.render.filepath = RENDER_PATH
+
+# Finding objects 
 scene.objects.keys()
 prop_name = 'full propeller - Standard Blade-1'
 motor_name = "KDE8218XF-120KV.001"
@@ -20,18 +28,15 @@ if motor_name in scene.objects.keys():
 else:
     motor = None
 
-
-
-
-path = r"C:\Users\mattg\OneDrive\Documents\Design\DesignRepo\anim_data.csv"
-data = np.genfromtxt(path,usecols=(0,1), delimiter=',')
+#loading data
+data = np.genfromtxt(DATA_PATH,usecols=(0,1), delimiter=',')
 angles = [i[1] for i in data]
 times = [i[0] for i in data]
 
 ### animation
 positions_prop = [( math.pi/2,0,i) for i in angles]
 positions_motor = [(math.pi,0,i) for i in angles]
-print(os.getcwd())
+
 # start with frame 0
 number_of_frame = 0  
 for i in range(len(positions_prop)):
@@ -50,3 +55,10 @@ for i in range(len(positions_prop)):
     
     # move next 10 frames forward - Blender will figure out what to do between this time
     number_of_frame += 1
+    
+    
+scene.frame_start = 0
+scene.frame_end = len(positions_prop) - 1
+#render
+if AUTO_RENDER:
+    bpy.ops.render.render(animation=True)
